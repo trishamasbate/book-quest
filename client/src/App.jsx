4811ce632c7486d21191029818d3ed9ffbbc30c2
trishamasbate <trisha.masbate@gmail.com>
2024-09-20 13:08:37 -0600

    Enhanced the App.jsx file to accommodate more functions.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Create an HTTP link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Middleware to attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // Retrieve the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Set up Apollo Client to use the authLink middleware before making GraphQL API requests
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    // Wrap the application in ApolloProvider to provide the Apollo Client to the React component tree
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
