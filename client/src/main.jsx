cebd14db53292ed0ae92b0300028fc5f731aba7e
trishamasbate <trisha.masbate@gmail.com>
2024-09-20 13:10:46 -0600

    Modified codes in landing page.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,31 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import App from './App.jsx';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 
+// Define the routes for the application
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    element: <App />, // Main application component
+    errorElement: <h1 className='display-2'>Wrong page!</h1>, // Error element for invalid routes
     children: [
       {
-        index: true,
-        element: <SearchBooks />
-      }, {
-        path: '/saved',
-        element: <SavedBooks />
+        index: true, // Default route
+        element: <SearchBooks /> // Component to render for the default route
+      }, 
+      {
+        path: '/saved', // Route for saved books
+        element: <SavedBooks /> // Component to render for the saved books route
       }
     ]
   }
-])
+]);
 
+// Render the application and provide the router configuration
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
